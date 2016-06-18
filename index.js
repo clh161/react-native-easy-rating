c71807d3d3bff5b80e0bdfd7c9f8a8d514861df6
jacob <clh161@gmail.com>
2016-06-19 01:52:29 +0800

    Added rating view and images

---
+++ index.js
@@ -0,0 +1,42 @@
+'use strict';
+import {StyleSheet, View, TouchableWithoutFeedback, Image} from "react-native";
+import React, {Component} from "react";
+
+class Rating extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            rating: this.props.rating ? this.props.rating : 0,
+            max: this.props.max ? this.props.max : 5,
+            iconWidth: this.props.iconWidth ? this.props.iconWidth : 36,
+            iconHeight: this.props.iconHeight ? this.props.iconHeight : 36,
+            iconSelected: this.props.iconSelected ? this.props.iconSelected : require('./images/icon_star_selected.png'),
+            iconUnselected: this.props.iconUnselected ? this.props.iconUnselected : require('./images/icon_star_unselected.png')
+        }
+    }
+
+    _onRate(rating) {
+        this.setState({rating});
+        if (this.props.onRate) {
+            this.props.onRate(rating)
+        }
+    }
+
+    render() {
+        var icons = [];
+        for (let i = 1; i <= this.state.max; i++) {
+            icons.push(<TouchableWithoutFeedback
+                style={{height:this.state.iconHeight,width:this.state.iconWidth}}
+                onPress={()=>this._onRate(i)}
+            >
+                <Image style={{height:this.state.iconHeight,width:this.state.iconWidth}}
+                       source={this.state.rating>=i?this.state.iconSelected:this.state.iconUnselected}/>
+            </TouchableWithoutFeedback>)
+        }
+        return <View style={[this.props.style,{flexDirection:'row'}]}>
+            {icons}
+        </View>
+    }
+}
+
+export default Rating;
