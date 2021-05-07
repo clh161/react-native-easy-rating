fc2de97860a2c252c63a095b6612efd7f30343c9
Jacob Ho <clh161@gmail.com>
2021-05-07 01:53:27 +0100

    functional component

--- index.js
+++ index.js
@@ -1,45 +1,38 @@
 'use strict';
-import {StyleSheet, View, TouchableWithoutFeedback, Image} from "react-native";
-import React, {Component} from "react";
+import {Image, TouchableWithoutFeedback, View} from "react-native";
+import React, {useState} from "react";
+import defaultIconSelected from './images/icon_star_selected.png'
+import defaultIconUnselected from './images/icon_star_unselected.png'
 
-class Rating extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            rating: this.props.rating ? this.props.rating : 0,
-            max: this.props.max ? this.props.max : 5,
-            iconWidth: this.props.iconWidth ? this.props.iconWidth : 36,
-            iconHeight: this.props.iconHeight ? this.props.iconHeight : 36,
-            iconSelected: this.props.iconSelected ? this.props.iconSelected : require('./images/icon_star_selected.png'),
-            iconUnselected: this.props.iconUnselected ? this.props.iconUnselected : require('./images/icon_star_unselected.png'),
-            editable: this.props.editable != null ? this.props.editable : true
-        }
-    }
+function Rating(props) {
+  const [internalRating, setInternalRating] = useState(props.rating ?? 0);
+  const rating = props.rating ?? internalRating
 
-    _onRate(rating) {
-        this.setState({rating});
-        if (this.props.onRate) {
-            this.props.onRate(rating)
-        }
-    }
+  const max = props.max ?? 5
+  const editable = props.editable ?? true
+  const iconWidth = props.iconWidth ?? 36
+  const iconHeight = props.iconHeight ?? 36
+  const iconSelected = props.iconSelected ?? defaultIconSelected
+  const iconUnselected = props.iconUnselected ?? defaultIconUnselected
 
-    render() {
-        var icons = [];
-        for (let i = 1; i <= this.state.max; i++) {
-            icons.push(<TouchableWithoutFeedback
-                disabled={!this.state.editable}
-                key={i}
-                style={{height:this.state.iconHeight,width:this.state.iconWidth}}
-                onPress={()=>this._onRate(i)}
-            >
-                <Image style={{height:this.state.iconHeight,width:this.state.iconWidth}}
-                       source={this.state.rating>=i?this.state.iconSelected:this.state.iconUnselected}/>
-            </TouchableWithoutFeedback>)
-        }
-        return <View style={[this.props.style,{flexDirection:'row'}]}>
-            {icons}
-        </View>
-    }
+  const icons = [];
+  for (let i = 1; i <= max; i++) {
+    icons.push(<TouchableWithoutFeedback
+      disabled={!editable}
+      key={i}
+      style={{height: iconHeight, width: iconWidth}}
+      onPress={() => {
+        setInternalRating(i);
+        props.onRate?.(i)
+      }}
+    >
+      <Image style={{height: iconHeight, width: iconWidth}}
+             source={rating >= i ? iconSelected : iconUnselected}/>
+    </TouchableWithoutFeedback>)
+  }
+  return <View style={[props.style, {flexDirection: 'row'}]}>
+    {icons}
+  </View>
 }
 
 export default Rating;
